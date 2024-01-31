02aa8947d73476bf1dc0072645b5dc7fd697edf8
Diego Hernandez <diegofootball19@gmail.com>
2024-01-31 11:04:48 -0600

    add aside with links

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,35 @@
-import { UserButton } from '@clerk/nextJs'
+import { UserButton } from "@clerk/nextJs";
+import Link from "next/link";
 
-const DashboardLayout = ( {children} ) => {
-    return <div className="h-screen w-screen relative">
-        <aside className="absolute w-48 top-0 left-0 h-full border-r border-r-black/10">
-            mood
-        </aside>
-        <div className="ml-[200px]">
-            <header className="h-16 border-b border-black/10"><div className="h-full w-full pr-6 flex items-center justify-end"> <UserButton/></div></header>
-            <div>{children}</div>
-        </div>
-        </div>
-}
+const links = [
+  { href: "/", label: "home" },
+  { href: "/journal", label: "journal" },
+];
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className="h-screen w-screen relative">
+      <aside className="absolute w-48 top-0 left-0 h-full border-r border-r-black/10">
+        <h2 className="capitalize text-3xl p-2">mood</h2>
+
+        <ul>
+          {links.map((link) => (
+            <li key={link.href} className="p-4 text text-xl capitalize">
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </aside>
+      <div className="ml-[200px]">
+        <header className="h-16 border-b border-black/10">
+          <div className="h-full w-full pr-6 flex items-center justify-end">
+            {" "}
+            <UserButton />
+          </div>
+        </header>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
 export default DashboardLayout;
