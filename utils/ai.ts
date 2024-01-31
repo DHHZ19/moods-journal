97931ca1d5baa9f58f3e466677badaba360b7a8b
Diego Hernandez <diegofootball19@gmail.com>
2024-01-30 21:21:53 -0600

    add openai

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,7 +1,7 @@
 import { OpenAI } from "langchain/llms/openai";
 import { StructuredOutputParser } from "langchain/output_parsers";
 import z from "zod";
-
+import { PromptTemplate } from "langchain/prompts";
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
     mood: z
@@ -16,11 +16,31 @@ const parser = StructuredOutputParser.fromZodSchema(
     color: z
       .string()
       .describe(
-        "a hexadecimal color code the represents the mood of the entry. Example #ffff11 for yellow representing happiness."
+        "a hexadecimal color code that represents the mood of the entry. Example #ffff11 for yellow representing happiness."
       ),
   })
 );
+
+const getPrompt = async (content) => {
+  const format_instructions = parser.getFormatInstructions();
+
+  const prompt = new PromptTemplate({
+    template:
+      "Analyze the following journal entry. Follow the instructions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}",
+    inputVariables: ["entry"],
+    partialVariables: { format_instructions },
+  });
+
+  const input = await prompt.format({
+    entry: content,
+  });
+
+  console.log(input);
+  return input;
+};
+
 export const analyze = async (prompt) => {
+  const input = await getPrompt(content);
   const model = new OpenAI({ temperature: 0, modelName: "gpt-3.5-turbo" });
   const result = await model.call(prompt);
   console.log(result);
