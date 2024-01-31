f05046063f0c90b175e0caed313bb5a1f97929e4
Diego Hernandez <diegofootball19@gmail.com>
2024-01-31 10:49:59 -0600

    update editor component

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -19,36 +19,9 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id);
-  const { mood, summary, color, subject, negative } = entry?.analysis;
-  const anaylisData = [
-    { name: "Summary", value: summary },
-    { name: "Subject", value: subject },
-    { name: "Mood", value: mood },
-    { name: "Negative", value: negative ? "True" : "False" },
-  ];
   return (
-    <div className="w-full h-full grid grid-cols-3">
-      <div className="col-span-2">
-        <Editor entry={entry} />
-      </div>
-      <div className="border-l border-black/10">
-        <div className="py-10 px-6" style={{ backgroundColor: color }}>
-          <h2 className="text-2xl">Analysis</h2>
-        </div>
-        <div>
-          <ul>
-            {anaylisData.map((data) => (
-              <li
-                key={data.name}
-                className="flex items-center justify-between p-4 border-t border-b border-black/10"
-              >
-                <span className="text-lg font-semibold">{data.name}</span>
-                <span className="p-2">{data.value}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+    <div className="w-full h-full">
+      <Editor entry={entry} />
     </div>
   );
 };
--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,6 +1,7 @@
 import { analyze } from "@/utils/ai";
 import { getUserByClerkID } from "@/utils/auth";
 import { prisma } from "@/utils/db";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export const PATCH = async (request: Request, { params }) => {
@@ -18,7 +19,7 @@ export const PATCH = async (request: Request, { params }) => {
     },
   });
   const analysis = await analyze(updatedEntry.content);
-  await prisma.analysis.upsert({
+  const updated = await prisma.analysis.upsert({
     where: {
       entryId: updatedEntry.id,
     },
@@ -28,5 +29,5 @@ export const PATCH = async (request: Request, { params }) => {
     },
     update: analysis,
   });
-  return NextResponse.json({ data: updatedEntry });
+  return NextResponse.json({ data: { ...updatedEntry, analysis: updated } });
 };
